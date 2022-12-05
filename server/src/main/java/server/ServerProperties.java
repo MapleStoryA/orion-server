6f27977abb38e0eb7ca39ca7295f20d99b70a76a
Adilson A <dilspedro@gmail.com>
2022-12-05 12:54:12 +0100

    Add 127.0.0.1 for local config

--- server/src/main/java/server/ServerProperties.java
+++ server/src/main/java/server/ServerProperties.java
@@ -9,44 +9,44 @@ import java.util.Properties;
  */
 public class ServerProperties {
 
-  private static final Properties props = new Properties();
-  private static final String[] toLoad = {
-      "dist/world.properties",
-      "dist/db.properties",
-      "dist/login.properties",
-      "dist/channel.properties"
-  };
+    private static final Properties props = new Properties();
+    private static final String[] toLoad = {
+            "dist/world.properties",
+            "dist/db.properties",
+            "dist/login.properties",
+            "dist/channel.properties"
+    };
 
-  private ServerProperties() {
-  }
-
-  static {
-    load();
-  }
-
-  public static String getProperty(String s) {
-    return props.getProperty(s);
-  }
-
-  public static void setProperty(String prop, String newInf) {
-    props.setProperty(prop, newInf);
-  }
-
-  public static String getProperty(String s, String def) {
-    return props.getProperty(s, def);
-  }
-
-
-  public static void load() {
-    for (String s : toLoad) {
-      FileReader fr;
-      try {
-        fr = new FileReader(s);
-        props.load(fr);
-        fr.close();
-      } catch (IOException ex) {
-        ex.printStackTrace();
-      }
+    private ServerProperties() {
     }
-  }
+
+    static {
+        load();
+    }
+
+    public static String getProperty(String s) {
+        return props.getProperty(s);
+    }
+
+    public static void setProperty(String prop, String newInf) {
+        props.setProperty(prop, newInf);
+    }
+
+    public static String getProperty(String s, String def) {
+        return props.getProperty(s, def);
+    }
+
+
+    public static void load() {
+        for (String s : toLoad) {
+            FileReader fr;
+            try {
+                fr = new FileReader(s);
+                props.load(fr);
+                fr.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
