920b111a7b0ed9ecbe540017061fcdeb05a60617
Adilson A <dilspedro@gmail.com>
2022-12-05 23:25:28 +0100

    Polishing LoginServer

---
+++ server/src/main/java/handling/login/GameServer.java
@@ -0,0 +1,42 @@
+package handling.login;
+
+import handling.MapleServerHandler;
+import handling.PacketProcessor;
+import handling.mina.MapleCodecFactory;
+import org.apache.mina.common.ByteBuffer;
+import org.apache.mina.common.IoAcceptor;
+import org.apache.mina.common.SimpleByteBufferAllocator;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.transport.socket.nio.SocketAcceptor;
+import org.apache.mina.transport.socket.nio.SocketAcceptorConfig;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class GameServer {
+
+    private final int port;
+
+    protected InetSocketAddress inetSocketAddress;
+    protected IoAcceptor acceptor;
+
+    public GameServer(int channel, int port, PacketProcessor.Mode mode) {
+        this.port = port;
+        ByteBuffer.setUseDirectBuffers(false);
+        ByteBuffer.setAllocator(new SimpleByteBufferAllocator());
+        acceptor = new SocketAcceptor();
+        final SocketAcceptorConfig cfg = new SocketAcceptorConfig();
+        cfg.getSessionConfig().setTcpNoDelay(true);
+        cfg.setDisconnectOnUnbind(true);
+        cfg.getFilterChain().addLast("codec", new ProtocolCodecFilter(new MapleCodecFactory()));
+
+        try {
+            inetSocketAddress = new InetSocketAddress(port);
+            acceptor.bind(inetSocketAddress, new MapleServerHandler(channel, false, PacketProcessor.getProcessor(mode)), cfg);
+            System.out.println("Listening on port " + port + ".");
+        } catch (IOException e) {
+            System.err.println("Binding to port " + port + " failed" + e);
+        }
+    }
+
+}
--- server/src/main/java/handling/login/LoginServer.java
+++ server/src/main/java/handling/login/LoginServer.java
@@ -21,7 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package handling.login;
 
-import com.mysql.jdbc.log.Log;
 import handling.MapleServerHandler;
 import handling.PacketProcessor;
 import handling.mina.MapleCodecFactory;
@@ -31,65 +30,46 @@ import org.apache.mina.common.SimpleByteBufferAllocator;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.transport.socket.nio.SocketAcceptor;
 import org.apache.mina.transport.socket.nio.SocketAcceptorConfig;
-import server.ServerProperties;
+import server.config.ServerConfig;
+import server.config.ServerEnvironment;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoginServer {
+public class LoginServer extends GameServer {
 
     public static final int PORT = 8484;
-    private static InetSocketAddress InetSocketadd;
-    private static IoAcceptor acceptor;
-    private static Map<Integer, Integer> load = new HashMap<Integer, Integer>();
-    private static String serverName, eventMessage;
-    private static byte flag;
-    private static int maxCharacters, userLimit, usersOn = 0;
-    private static boolean finishedShutdown = true, adminOnly = false;
+    private Map<Integer, Integer> load = new HashMap<>();
+    private String serverName, eventMessage;
+    private byte flag;
+    private int userLimit, usersOn = 0;
+    private boolean finishedShutdown = true, adminOnly;
 
     private static LoginServer INSTANCE;
 
-    public LoginServer(){
-        userLimit = Integer.parseInt(ServerProperties.getProperty("login.userlimit"));
-        serverName = ServerProperties.getProperty("login.serverName");
-        eventMessage = ServerProperties.getProperty("login.eventMessage");
-        flag = Byte.parseByte(ServerProperties.getProperty("login.flag"));
-        adminOnly = Boolean.parseBoolean(ServerProperties.getProperty("world.admin", "false"));
-        maxCharacters = Integer.parseInt(ServerProperties.getProperty("login.maxCharacters"));
-
-        ByteBuffer.setUseDirectBuffers(false);
-        ByteBuffer.setAllocator(new SimpleByteBufferAllocator());
-
-        acceptor = new SocketAcceptor();
-        final SocketAcceptorConfig cfg = new SocketAcceptorConfig();
-        cfg.getSessionConfig().setTcpNoDelay(true);
-        cfg.setDisconnectOnUnbind(true);
-        cfg.getFilterChain().addLast("codec", new ProtocolCodecFilter(new MapleCodecFactory()));
-
-        try {
-            InetSocketadd = new InetSocketAddress(PORT);
-            acceptor.bind(InetSocketadd, new MapleServerHandler(-1, false, PacketProcessor.getProcessor(PacketProcessor.Mode.LOGINSERVER)), cfg);
-            System.out.println("Listening on port " + PORT + ".");
-        } catch (IOException e) {
-            System.err.println("Binding to port " + PORT + " failed" + e);
-        }
+    public LoginServer(ServerConfig config) {
+        super(-1, PORT, PacketProcessor.Mode.LOGINSERVER);
+        userLimit = Integer.parseInt(config.getProperty("login.userlimit"));
+        serverName = config.getProperty("login.serverName");
+        eventMessage = config.getProperty("login.eventMessage");
+        flag = Byte.parseByte(config.getProperty("login.flag"));
+        adminOnly = Boolean.parseBoolean(config.getProperty("world.admin", "false"));
     }
 
     public static synchronized LoginServer getInstance() {
         if (INSTANCE == null) {
-            INSTANCE = new LoginServer();
+            INSTANCE = new LoginServer(ServerEnvironment.getConfig());
         }
         return INSTANCE;
     }
 
-
-    public final void addChannel(final int channel) {
+    public void addChannel(final int channel) {
         load.put(channel, 0);
     }
 
-    public final void removeChannel(final int channel) {
+    public void removeChannel(final int channel) {
         load.remove(channel);
     }
 
@@ -99,40 +79,32 @@ public class LoginServer {
         }
         System.out.println("Shutting down login...");
         acceptor.unbindAll();
-        finishedShutdown = true; //nothing. lol
+        finishedShutdown = true;
     }
 
     public final String getServerName() {
         return serverName;
     }
 
-    public final String getEventMessage() {
+    public final String getServerEventMessage() {
         return eventMessage;
     }
 
-    public final byte getFlag() {
+    public final byte getServerFlag() {
         return flag;
     }
 
-    public final int getMaxCharacters() {
-        return maxCharacters;
-    }
-
-    public final Map<Integer, Integer> getLoad() {
+    public final Map<Integer, Integer> getServerLoad() {
         return load;
     }
 
-    public void setLoad(final Map<Integer, Integer> load_, final int usersOn_) {
-        load = load_;
-        usersOn = usersOn_;
+    public void setLoad(final Map<Integer, Integer> load, final int usersOn) {
+        this.load = load;
+        this.usersOn = usersOn;
     }
 
-    public final void setEventMessage(final String newMessage) {
-        eventMessage = newMessage;
-    }
-
-    public final void setFlag(final byte newflag) {
-        flag = newflag;
+    public final void setFlag(final byte flag) {
+        this.flag = flag;
     }
 
     public final int getUserLimit() {
@@ -141,14 +113,6 @@ public class LoginServer {
 
     public final int getUsersOn() {
         return usersOn;
-    }
-
-    public final void setUserLimit(final int newLimit) {
-        userLimit = newLimit;
-    }
-
-    public final int getNumberOfSessions() {
-        return acceptor.getManagedSessions(InetSocketadd).size();
     }
 
     public final boolean isAdminOnly() {
