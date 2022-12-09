674f01a697ad1968bf74a8afe9f605f705e250e4
Adilson A <dilspedro@gmail.com>
2022-12-09 23:40:56 +0100

    add simple doc in password encryptor

--- server/src/main/java/database/PasswordEncryptor.java
+++ server/src/main/java/database/PasswordEncryptor.java
@@ -8,17 +8,16 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
+/**
+ * Simple class to hash the password with Bcrypt.
+ * <p>
+ * Salt = SHA-256(uuid + seed)
+ * hashed password = bcrypt(Salt + password)
+ * decrypted password = bcrypt_verify((Salt + raw password), hashed password)
+ **/
 @Slf4j
 public class PasswordEncryptor {
 
-
-    public String generateSalt(String value) {
-        try {
-            return createSha256(value);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public String encrypt(String password, String salt) {
         return BCrypt.withDefaults().hashToString(10, salt.concat(password).toCharArray());
@@ -28,16 +27,24 @@ public class PasswordEncryptor {
         return BCrypt.verifyer().verify(salt.concat(rawPassword).toCharArray(), hashedPassword).verified;
     }
 
-    private String createSha256(String value) throws NoSuchAlgorithmException {
+    private String createSha256(String seed) throws NoSuchAlgorithmException {
         var md = MessageDigest.getInstance("SHA-256");
         var uuid = UUID.randomUUID().toString();
         md.update(uuid.getBytes());
-        byte[] bytes = md.digest(value.getBytes(StandardCharsets.UTF_8));
+        byte[] bytes = md.digest(seed.getBytes(StandardCharsets.UTF_8));
         var sb = new StringBuilder();
         for (int i = 0; i < bytes.length; i++) {
             sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
         }
         return sb.toString();
+    }
+
+    private String generateSalt(String seed) {
+        try {
+            return createSha256(seed);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
