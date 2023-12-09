de39c691ca4f4c505c20070bcbd49c10e1467989
Adilson A <dilspedro@gmail.com>
2023-12-09 01:14:04 +0100

    Add GainMesosCommand

---
+++ server/src/main/java/client/commands/GainMesosCommand.java
@@ -0,0 +1,23 @@
+package client.commands;
+
+import client.MapleClient;
+
+public class GainMesosCommand implements Command {
+    @Override
+    public void execute(MapleClient c, String[] args) {
+        if (args.length < 1) {
+            sendSyntaxMessage(c);
+        }
+
+        c.getPlayer().gainMeso(Integer.valueOf(args[0]), true);
+    }
+
+    @Override
+    public String getTrigger() {
+        return "mesos";
+    }
+
+    private void sendSyntaxMessage(MapleClient client) {
+        client.getPlayer().dropMessage(5, "[Syntax] !" + getTrigger() + " <Amount>");
+    }
+}
