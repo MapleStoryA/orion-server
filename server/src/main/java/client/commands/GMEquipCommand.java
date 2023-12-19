f4d0534093d72eb9c8ab22bbd2ef9a860311783c
Dil A <9415675+devdilson@users.noreply.github.com>
2023-12-19 22:45:12 +0100

    Polishing2 (#61)
    * Add GMEquip command
    
    * Fix warnings

---
+++ server/src/main/java/client/commands/GMEquipCommand.java
@@ -0,0 +1,30 @@
+package client.commands;
+
+import client.MapleClient;
+import client.inventory.MapleInventoryType;
+import server.MapleInventoryManipulator;
+
+public class GMEquipCommand implements Command {
+    @Override
+    public void execute(MapleClient c, String[] args) {
+        addItem(c, 1002140); // Invincible Hat
+        addItem(c, 1042003); // Plain Suit
+        addItem(c, 1062007); // Plain Suit pants
+        addItem(c, 1322013); // Secret Agent case
+    }
+
+    private static void addItem(MapleClient c, int itemId) {
+        if (c.getPlayer()
+                .getInventory(MapleInventoryType.EQUIP)
+                .listById(itemId)
+                .isEmpty()) {
+            MapleInventoryManipulator.addById(
+                    c, itemId, (short) 1, c.getPlayer().getName());
+        }
+    }
+
+    @Override
+    public String getTrigger() {
+        return "gmequip";
+    }
+}
