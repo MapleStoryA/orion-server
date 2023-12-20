c685948881233306f3f4a294b5ca04f2b79903da
Adilson A <dilspedro@gmail.com>
2023-12-20 17:43:40 +0100

    Fix wrong function

--- server/config/scripts/event/3rdjob.js
+++ server/config/scripts/event/3rdjob.js
@@ -23,30 +23,30 @@ function setClassVars(player) {
     var returnMapId;
     var monsterId;
     var mapId;
-    if (player.getJobValue().equals(MapleJob.FP_WIZARD) ||
-        player.getJobValue().equals(MapleJob.IL_WIZARD) ||
-        player.getJobValue().equals(MapleJob.CLERIC)) {
+    if (player.getJob().equals(MapleJob.FP_WIZARD) ||
+        player.getJob().equals(MapleJob.IL_WIZARD) ||
+        player.getJob().equals(MapleJob.CLERIC)) {
         mapId = 108010200;
         returnMapId = 100040106;
         monsterId = 9001001;
-    } else if (player.getJobValue().equals(MapleJob.FIGHTER) ||
-        player.getJobValue().equals(MapleJob.PAGE) ||
-        player.getJobValue().equals(MapleJob.SPEARMAN)) {
+    } else if (player.getJob().equals(MapleJob.FIGHTER) ||
+        player.getJob().equals(MapleJob.PAGE) ||
+        player.getJob().equals(MapleJob.SPEARMAN)) {
         mapId = 108010300;
         returnMapId = 105070001;
         monsterId = 9001000;
-    } else if (player.getJobValue().equals(MapleJob.ASSASSIN) ||
-        player.getJobValue().equals(MapleJob.BANDIT)) {
+    } else if (player.getJob().equals(MapleJob.ASSASSIN) ||
+        player.getJob().equals(MapleJob.BANDIT)) {
         mapId = 108010400;
         returnMapId = 107000402;
         monsterId = 9001003;
-    } else if (player.getJobValue().equals(MapleJob.HUNTER) ||
-        player.getJobValue().equals(MapleJob.CROSSBOWMAN)) {
+    } else if (player.getJob().equals(MapleJob.HUNTER) ||
+        player.getJob().equals(MapleJob.CROSSBOWMAN)) {
         mapId = 108010100;
         returnMapId = 105040305;
         monsterId = 9001002;
-    } else if (player.getJobValue().equals(MapleJob.BRAWLER) ||
-        player.getJobValue().equals(MapleJob.GUNSLINGER)) {
+    } else if (player.getJob().equals(MapleJob.BRAWLER) ||
+        player.getJob().equals(MapleJob.GUNSLINGER)) {
         mapId = 108010500;
         returnMapId = 105040305;
         monsterId = 9001008;
