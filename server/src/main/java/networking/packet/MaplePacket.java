24549c491103c63fe528772cd43d96e77ce0b7cd
Adilson A <dilspedro@gmail.com>
2023-12-21 00:09:47 +0100

    Remove unused class

--- server/src/main/java/networking/packet/MaplePacket.java
+++
@@ -1,9 +0,0 @@
-package networking.packet;
-
-public interface MaplePacket extends java.io.Serializable {
-    byte[] getBytes();
-
-    Runnable getOnSend();
-
-    void setOnSend(Runnable onSend);
-}
