9d5d0c4ca782cbdac740b9814046890407ed9f5f
Adilson A <dilspedro@gmail.com>
2023-12-21 11:24:49 +0100

    Add GM equip command

--- server/src/main/java/client/commands/AdminCommands.java
+++ server/src/main/java/client/commands/AdminCommands.java
@@ -3481,4 +3481,26 @@ public class AdminCommands {
             return 0;
         }
     }
+
+    @Api(description = "Gives the player the gm equips")
+    public static class GMEquip implements Command {
+        @Override
+        public int execute(MapleClient c, String[] args) {
+            addItem(c, 1002140); // Invincible Hat
+            addItem(c, 1042003); // Plain Suit
+            addItem(c, 1062007); // Plain Suit pants
+            addItem(c, 1322013); // Secret Agent case
+            return 1;
+        }
+
+        private static void addItem(MapleClient c, int itemId) {
+            if (c.getPlayer()
+                    .getInventory(MapleInventoryType.EQUIP)
+                    .listById(itemId)
+                    .isEmpty()) {
+                MapleInventoryManipulator.addById(
+                        c, itemId, (short) 1, c.getPlayer().getName());
+            }
+        }
+    }
 }
