84a5de65ab36d3e590aedbe81a8399a9393043ab
Adilson A <dilspedro@gmail.com>
2023-12-21 10:31:33 +0100

    Fix warp command

--- server/src/main/java/client/commands/WarpCommand.java
+++ server/src/main/java/client/commands/WarpCommand.java
@@ -4,40 +4,24 @@ import client.MapleCharacter;
 import client.MapleClient;
 import handling.channel.ChannelServer;
 import handling.world.WorldServer;
-import java.util.LinkedList;
-import java.util.List;
-import server.maps.MapleMap;
 import tools.helper.Scripting;
 
 @Scripting
 class WarpCommand implements Command {
     @Override
     public void execute(MapleClient c, String[] args) {
-        MapleCharacter victim = null; // =
-        List<String> possibility = new LinkedList<>();
-        StringBuilder sb = new StringBuilder();
+        MapleCharacter victim = null;
         for (ChannelServer ch : WorldServer.getInstance().getAllChannels()) {
             for (MapleCharacter chr : ch.getPlayerStorage().getAllCharacters()) {
-                if (chr.getName().toLowerCase().contains(args[0].toLowerCase()) && victim == null) {
+                if (chr.getName().toLowerCase().contains(args[0].toLowerCase())) {
                     victim = chr;
-                    possibility.add(chr.getName());
-                } else if (chr.getName().contains(args[0]) && victim != null) {
-                    // key++;
-                    possibility.add(chr.getName());
                 }
             }
         }
-        if (possibility.size() > 1) {
-            sb.append("There were more than 1 player found, do !warp : ").append(possibility);
-            c.getPlayer().dcolormsg(5, sb.toString());
+        if (victim != null) {
+            c.getPlayer().changeMap(victim.getMap(), victim.getPosition());
         } else {
-            if (victim != null) {
-                MapleMap target = WorldServer.getInstance()
-                        .getChannel(c.getChannel())
-                        .getMapFactory()
-                        .getMap(Integer.parseInt(args[1]));
-                victim.changeMap(target, target.getPortal(0));
-            }
+            c.getPlayer().dcolormsg(5, "The player was not found online");
         }
     }
 
