9511cfa3c39d037ae27d255e0ed7480ae5cd4a98
Adilson A <dilspedro@gmail.com>
2023-12-21 13:12:33 +0100

    Moving things around

--- server/src/main/java/client/AttackPair.java
+++ server/src/main/java/client/AttackPair.java
@@ -1,10 +1,8 @@
 package client;
 
 import java.util.List;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import tools.collection.Pair;
 
 @Slf4j
-@Getter
 public record AttackPair(int objectId, List<Pair<Integer, Boolean>> attack) {}
